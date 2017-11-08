64b6a56bf0e18cdd4e911124e51c16b4bf36f3bd
MassiDelta <massidelpizzo@gmail.com>
2017-11-08 15:06:19 +0000

    finished list-practice.py

--- list-practice.py
+++ list-practice.py
@@ -6,7 +6,7 @@ If you're correct, you'll get a message in the terminal saying "Your string_list
 
 # On the line below, create a list named string_list containing 7 strings
 
-
+string_list = ['thing', 'thought', 'concept', 'figment', 'idea', 'prototype', 'plan']
 
 
 
